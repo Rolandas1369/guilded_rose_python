f7b0e462562e3feb2d98509c8d28d20cf9db5911
rolandas1369 <rolandaswb@gmail.com>
2020-07-02 20:37:22 +0300

    test negative quality

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -35,8 +35,11 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose.update_quality()
         self.assertEqual(0, items[0].quality)
 
-
-    
+    def test_aged_brie_value(self):
+        items = [Item("Aged Brie", 10, 10)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(11, items[0].quality)
 
 if __name__ == '__main__':
     unittest.main()
