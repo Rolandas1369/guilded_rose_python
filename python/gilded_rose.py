81a27bd2a3ce823b972779abafecbfebf1f912e2
Rolandas1369 <rolandaswb@gmail.com>
2020-07-05 20:21:40 +0300

    add aged brie if case

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -4,22 +4,27 @@
 # 3. After a consideration I need to test code 100% before refractoring.
 # 4. After test 100% pass, I need to refactor tests.
 
+
 class GildedRose(object):
 
     def __init__(self, items):
         self.items = items
-    
+
     def check_quantity(self, item):
         if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality -= 1
+            if item.name != "Sulfuras, Hand of Ragnaros":
+                item.quality -= 1
         return item.quality
-    
-    
 
+    # Aged Brie gets quality up as it gets older
 
     def update_quality(self):
         for item in self.items:
+
+            # Aged Brie value increase by one as day passes
+            if item.name == "Aged Brie" and item.sell_in < 50:
+                item.quality += 1
+
             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
                 self.check_quantity(item)
 
@@ -41,9 +46,7 @@ class GildedRose(object):
                         self.check_quantity(item)
                     else:
                         item.quality = 0
-                else:
-                    if item.quality < 50:
-                        item.quality += 1
+
 
 class Item:
     def __init__(self, name, sell_in, quality):
