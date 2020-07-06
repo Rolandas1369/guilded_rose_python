e7c5f9044f9b8edd4c2e894920800e5500ec45b9
Rolandas1369 <rolandaswb@gmail.com>
2020-07-06 12:38:59 +0300

    empty update quality function

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -3,6 +3,7 @@
 # 2. One function was moved before testing.
 # 3. After a consideration I need to test code 100% before refractoring.
 # 4. After test 100% pass, I need to refactor tests.
+# 5. After doing this refactoring a little bit I think in this case rewriting all update quality is good choice
 
 
 class GildedRose(object):
@@ -10,42 +11,12 @@ class GildedRose(object):
     def __init__(self, items):
         self.items = items
 
-    def check_quantity(self, item):
-        if item.quality > 0:
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.quality -= 1
-        return item.quality
-
     # Aged Brie gets quality up as it gets older
 
     def update_quality(self):
         for item in self.items:
 
-            # Aged Brie value increase by one as day passes
-            if item.name == "Aged Brie" and item.sell_in < 50:
-                item.quality += 1
-
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                self.check_quantity(item)
-
-            else:
-                if item.quality < 50:
-                    item.quality += 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality += 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality += 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in -= 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        self.check_quantity(item)
-                    else:
-                        item.quality = 0
+            
 
 
 class Item:
--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -4,80 +4,79 @@ import unittest
 
 from gilded_rose import Item, GildedRose
 
+
 class GildedRoseTest(unittest.TestCase):
 
     def test_quality_decreases_by_1(self):
-        names_list = ['+5 Dexterity Vest', "Elixir of the Mongoose"
-        , "Conjured Mana Cake"]
+        names_list = ['+5 Dexterity Vest',
+                      "Elixir of the Mongoose", "Conjured Mana Cake"]
         items = [Item(name, 45, 45) for name in names_list]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         for item in range(len(items)):
             self.assertEqual(44, items[item].quality)
-    
+
     def test_sell_in_decreases_by_1(self):
         names_list = ['+5 Dexterity Vest', "Elixir of the Mongoose",
-        "Backstage passes to a TAFKAL80ETC concert", "Conjured Mana Cake"]
+                      "return item.quality", "Conjured Mana Cake"]
         items = [Item(name, 45, 45) for name in names_list]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         for item in range(len(items)):
-            self.assertEqual(44, items[item].sell_in)
-    
+            self.assertEqual(44, items[item].sell_in,
+                             f"Item failing {items[0].name}")
+
     def test_negative_sell_in_returns_0(self):
         names_list = ['+5 Dexterity Vest', "Elixir of the Mongoose",
-         "Sulfuras, Hand of Ragnaros", "Backstage passes to a TAFKAL80ETC concert", "Conjured Mana Cake"]
+                      "Sulfuras, Hand of Ragnaros", "Backstage passes to a TAFKAL80ETC concert", "Conjured Mana Cake"]
         items = [Item(name, -1, 0) for name in names_list]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         for item in range(len(items)):
             self.assertEqual(0, items[item].quality)
-        
 
     def test_quality_cant_increase_more_than_50(self):
         names_list = ['+5 Dexterity Vest', 'Aged Brie', "Elixir of the Mongoose",
-         "Sulfuras, Hand of Ragnaros", "Backstage passes to a TAFKAL80ETC concert", "Conjured Mana Cake"]
+                      "Sulfuras, Hand of Ragnaros", "Backstage passes to a TAFKAL80ETC concert", "Conjured Mana Cake"]
         items = [Item(name, 50, 50) for name in names_list]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         for item in range(len(items)):
             self.assertTrue(50 >= items[item].quality)
-    
+
     def test_representation_of_item(self):
         item = Item("Name", 50, 60)
         self.assertEqual(str(item), "Name, 50, 60")
-    
-    def test_backstage_pass(self):
-        items = [Item("Backstage passes to a TAFKAL80ETC concert", 10, 10)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality() 
-        self.assertEqual(12, items[0].quality)
-    
+
+    # def test_backstage_pass(self):
+    #     items = [Item("Backstage passes to a TAFKAL80ETC concert", 10, 10)]
+    #     gilded_rose = GildedRose(items)
+    #     gilded_rose.update_quality()
+    #     self.assertEqual(12, items[0].quality)
+
     def test_backstage_value_at_day_below_eleven(self):
         items = [Item("Backstage passes to a TAFKAL80ETC concert", 10, 10)]
         gilded_rose = GildedRose(items)
-        gilded_rose.update_quality() 
+        gilded_rose.update_quality()
         self.assertEqual(12, items[0].quality)
-    
+
     def test_backstage_value_at_day_below_six(self):
         items = [Item("Backstage passes to a TAFKAL80ETC concert", 3, 10)]
         gilded_rose = GildedRose(items)
-        gilded_rose.update_quality() 
+        gilded_rose.update_quality()
         self.assertEqual(13, items[0].quality)
-    
+
     def test_backstage_at_day_zero(self):
         items = [Item("Backstage passes to a TAFKAL80ETC concert", 0, 10)]
         gilded_rose = GildedRose(items)
-        gilded_rose.update_quality() 
+        gilded_rose.update_quality()
         self.assertEqual(0, items[0].quality)
-    
+
     def test_aged_bried_sell_in_is_negative(self):
         items = [Item("Aged Brie", -2, 10)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEqual(items[0].quality, 12)
-        
-
 
 
 if __name__ == '__main__':
