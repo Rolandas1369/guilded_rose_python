9a0c4530aa332d1cb7a61c14117b2b5833ea99ab
Rolandas1369 <rolandaswb@gmail.com>
2020-07-06 13:15:21 +0300

    add quality and sell in decrese by on

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -3,7 +3,7 @@
 # 2. One function was moved before testing.
 # 3. After a consideration I need to test code 100% before refractoring.
 # 4. After test 100% pass, I need to refactor tests.
-# 5. After doing this refactoring a little bit I think in this case rewriting all update quality is good choice
+# 5. After doing this refactoring a little bit I think in this case rewriting all update quality is good
 
 
 class GildedRose(object):
@@ -12,11 +12,23 @@ class GildedRose(object):
         self.items = items
 
     # Aged Brie gets quality up as it gets older
+    def decrease_quality(self, item):
+        item.quality -= 1
+        return item.quality
+
+    def decrease_sell_in(self, item):
+        item.sell_in -= 1
+        return item.sell_in
 
     def update_quality(self):
-        for item in self.items:
 
-            
+        for item in self.items:
+            self.decrease_quality(item)
+            self.decrease_sell_in(item)
+
+    def update_item(self):
+
+        print('ok')
 
 
 class Item:
